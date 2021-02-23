83e06f79f64bfe74d4963abdf2146137775b6a5e
Miguel Chiau <chiau.miguel@gmail.com>
2021-02-23 13:18:08 -0800

    first commit 2.0

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function about() {
+    return (
+        <div>
+            <head>
+                <title>
+                    About
+                </title>
+
+            </head>
+            <h1>
+                This is the About Page
+            </h1>
+        </div>
+    )
+}
